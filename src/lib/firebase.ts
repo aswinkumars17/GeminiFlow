9858bd0f1799a22da8934c871e017a2907d6a7db
AswinKumarS17 <saswinkumar17@gmail.com>
2025-09-05 11:26:22 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  projectId: 'geminiflow-a5d2e',
-  appId: '1:25065511329:web:9f27cece84f1e9d487d3e0',
-  storageBucket: 'geminiflow-a5d2e.firebasestorage.app',
-  apiKey: 'AIzaSyBFiqaSXQX95aApyENoI0Vfft62GKEcIDg',
-  authDomain: 'geminiflow-a5d2e.firebaseapp.com',
-  messagingSenderId: '25065511329',
+  "projectId": "geminiflow-a5d2e",
+  "appId": "1:25065511329:web:9f27cece84f1e9d487d3e0",
+  "storageBucket": "geminiflow-a5d2e.firebasestorage.app",
+  "apiKey": "AIzaSyBFiqaSXQX95aApyENoI0Vfft62GKEcIDg",
+  "authDomain": "geminiflow-a5d2e.firebaseapp.com",
+  "messagingSenderId": "25065511329"
 };
 
 // Initialize Firebase
